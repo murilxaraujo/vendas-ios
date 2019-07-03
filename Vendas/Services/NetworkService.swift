2714e9a5113b90bb8cf8de945a114669d44b1a4f
Murilo Araujo <murilo@mribeiro.com.br>
2019-07-03 16:21:34 -0300

    Create NetworkService.swift

---
+++ Vendas/Services/NetworkService.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkService.swift
+//  Vendas
+//
+//  Created by Murilo Araujo on 03/07/19.
+//  Copyright © 2019 Murilo Araujo. All rights reserved.
+//
+
+import Foundation
+
+class NetworkService {    
+    
+    init() {
+        
+    }
+}
