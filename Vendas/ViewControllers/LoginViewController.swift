9ceb4e803a0352913ec04c5e780b022c4e62a137
Murilo Araujo <murilo@mribeiro.com.br>
2019-07-05 17:05:34 -0300

    Finished the interface of the login page

--- Vendas/ViewControllers/LoginViewController.swift
+++ Vendas/ViewControllers/LoginViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import MaterialComponents.MaterialTextFields
+import MaterialComponents.MaterialButtons
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
     
     let logoBackgroundView: UIView = {
         let view = UIView()
@@ -19,19 +21,58 @@ class LoginViewController: UIViewController {
     
     let loginLabel: UILabel = {
         let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Login"
+        label.translatesAutoresizingMaskIntoConstraints = false
         label.textColor = .white
+        label.font = UIFont.boldSystemFont(ofSize: 34.0)
         return label
     }()
+    
+    let userNameTextField: MDCTextField = {
+        let tf = MDCTextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.placeholder = "Usuário"
+        return tf
+    }()
+    let usernameTextFieldController: MDCTextInputControllerFilled
+    
+    let senhaTextField: MDCTextField = {
+        let tf = MDCTextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.placeholder = "Senha"
+        tf.isSecureTextEntry = true
+        return tf
+    }()
+    let senhaTextFieldController: MDCTextInputControllerFilled
+    
+    let loginButton: MDCButton = {
+        let button = MDCButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Entrar", for: .normal)
+        button.setBackgroundColor(UIColor(red:0.25, green:0.32, blue:0.71, alpha:1.0))
+        return button
+    }()
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        usernameTextFieldController = MDCTextInputControllerFilled(textInput: userNameTextField)
+        senhaTextFieldController = MDCTextInputControllerFilled(textInput: senhaTextField)
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        setupViewInterface()
     }
     
     func setupViewInterface() {
+        self.view.backgroundColor = .white
+        
+        
         self.view.addSubview(logoBackgroundView)
         logoBackgroundView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
         logoBackgroundView.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
@@ -41,6 +82,26 @@ class LoginViewController: UIViewController {
         logoBackgroundView.addSubview(loginLabel)
         loginLabel.leftAnchor.constraint(equalTo: logoBackgroundView.leftAnchor, constant: 20).isActive = true
         loginLabel.bottomAnchor.constraint(equalTo: logoBackgroundView.bottomAnchor, constant: -20).isActive = true
+        
+        self.view.addSubview(userNameTextField)
+        userNameTextField.topAnchor.constraint(equalTo: logoBackgroundView.bottomAnchor, constant: 20).isActive = true
+        userNameTextField.leftAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leftAnchor, constant: 20).isActive = true
+        userNameTextField.rightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.rightAnchor, constant: -20).isActive = true
+        userNameTextField.delegate = self
+        
+        self.view.addSubview(senhaTextField)
+        senhaTextField.topAnchor.constraint(equalTo: userNameTextField.bottomAnchor, constant: 0).isActive = true
+        senhaTextField.leftAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leftAnchor, constant: 20).isActive = true
+        senhaTextField.rightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.rightAnchor, constant: -20).isActive = true
+        senhaTextField.delegate = self
+        
+        self.view.addSubview(loginButton)
+        loginButton.topAnchor.constraint(equalTo: senhaTextField.bottomAnchor, constant: 0).isActive = true
+        loginButton.rightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.rightAnchor, constant: -20).isActive = true
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
 
 }
