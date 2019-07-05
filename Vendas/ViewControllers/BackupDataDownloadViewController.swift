96d0f43d9ae598b55d1fa2d5b78df568e9031a2e
Murilo Araujo <murilo@mribeiro.com.br>
2019-07-05 17:40:26 -0300

    Created backup date view controller

---
+++ Vendas/ViewControllers/BackupDataDownloadViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BackupDataDownloadViewController.swift
+//  Vendas
+//
+//  Created by Murilo Araujo on 05/07/19.
+//  Copyright © 2019 Murilo Araujo. All rights reserved.
+//
+
+import UIKit
+
+class BackupDataDownloadViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
