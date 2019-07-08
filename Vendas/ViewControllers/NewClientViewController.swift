c5951438e725db28a54163c8b63b39af3d71e478
Murilo Araujo <murilo@mribeiro.com.br>
2019-07-08 17:46:54 -0300

    Created new client view controller

---
+++ Vendas/ViewControllers/NewClientViewController.swift
@@ -0,0 +1,22 @@
+//
+//  NewClientViewController.swift
+//  Vendas
+//
+//  Created by Murilo Araujo on 08/07/19.
+//  Copyright © 2019 Murilo Araujo. All rights reserved.
+//
+
+import UIKit
+
+class NewClientViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupViewElements()
+    }
+
+    func setupViewElements() {
+        self.view.backgroundColor = .white
+    }
+}
