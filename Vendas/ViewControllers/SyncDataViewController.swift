c440fe08c3e1429bd21a723e25bda8adebbe8c1d
Murilo Araujo <murilo@mribeiro.com.br>
2019-07-08 17:46:38 -0300

    Created data sync view controller

---
+++ Vendas/ViewControllers/SyncDataViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SyncDataViewController.swift
+//  Vendas
+//
+//  Created by Murilo Araujo on 08/07/19.
+//  Copyright © 2019 Murilo Araujo. All rights reserved.
+//
+
+import UIKit
+
+class SyncDataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupViewElements()
+    }
+    
+    func setupViewElements() {
+        self.view.backgroundColor = .white
+    }
+}
