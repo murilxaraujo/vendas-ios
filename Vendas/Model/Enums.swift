96a8cd1451993ac91231039bebdf5d92337f1118
Murilo Araujo <murilo@mribeiro.com.br>
2019-07-11 13:27:05 -0300

    Turned the Enums into separate file

---
+++ Vendas/Model/Enums.swift
@@ -0,0 +1,19 @@
+//
+//  Enums.swift
+//  Vendas
+//
+//  Created by Murilo Araujo on 11/07/19.
+//  Copyright © 2019 Murilo Araujo. All rights reserved.
+//
+
+enum NetworkServiceErrors: Error {
+    case valueisNil
+}
+
+enum LoginError: Error {
+    case userNotFound
+    case blockedUser
+    case notAttachedSeller
+    case wrongPassword
+    case networkError
+}
