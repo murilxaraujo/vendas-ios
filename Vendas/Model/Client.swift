bf8059dd619c970dc17cf23ee95aff627229a879
Murilo Araujo <muernandes@icloud.com>
2019-08-06 09:13:05 -0300

    added convenience initializer in the Client model class

--- Vendas/Model/Client.swift
+++ Vendas/Model/Client.swift
@@ -12,4 +12,12 @@ import RealmSwift
 class Client: Object {
     @objc dynamic var id: String = ""
     @objc dynamic var name: String = ""
+    @objc dynamic var telefone: String = ""
+    
+    convenience init(id: String, name: String, telefone: String) {
+        self.init()
+        self.id = id
+        self.name = name
+        self.telefone = telefone
+    }
 }
